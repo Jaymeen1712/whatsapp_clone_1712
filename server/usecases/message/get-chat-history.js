2693baf852ffe35a95ba8bdbd4ee30dd7af23c50
Jaymeen1712 <jaymeensonara@gmail.com>
2023-10-21 21:40:59 +0530

    changes in chat API

--- server/usecases/message/get-chat-history.js
+++ server/usecases/message/get-chat-history.js
@@ -1,37 +1,35 @@
-module.exports = function makeGetChatHistory({ getChatHistorydb, getSingleUserdb}) {
+module.exports = function makeGetChatHistory({
+  getChatHistorydb,
+  getSingleUserdb,
+}) {
   return async function getChatHistory({ params }) {
     const { userId } = params;
 
     const result = await getChatHistorydb({ userId });
 
     if (result[0]) {
-      const uniqueChatInfo = chatHistoryResponse?.data?.chats.reduce(
-        (result, chat) => {
-          const otherUserId =
-            chat.senderId === currentUserData.uuid
-              ? chat.receiverId
-              : chat.senderId;
-          const message = chat.message;
+      const uniqueChatInfo = result[0].reduce((result, chat) => {
+        const otherUserId =
+          chat.senderId === userId ? chat.receiverId : chat.senderId;
+        const message = chat.message;
 
-          const existingChatIndex = result.findIndex(
-            (chatInfo) => chatInfo.id === otherUserId
-          );
+        const existingChatIndex = result.findIndex(
+          (chatInfo) => chatInfo.id === otherUserId
+        );
 
-          if (existingChatIndex === -1) {
-            result.push({ id: otherUserId, message });
-          } else {
-            result[existingChatIndex].message = message;
-          }
+        if (existingChatIndex === -1) {
+          result.push({ id: otherUserId, message });
+        } else {
+          result[existingChatIndex].message = message;
+        }
 
-          return result;
-        },
-        []
-      );
+        return result;
+      }, []);
 
       const chatInfo = await Promise.all(
         uniqueChatInfo.map(async (chat) => {
           const response = await getSingleUserdb({ uuid: chat.id });
-          return { ...chat, ...response.data.users[0] };
+          return { ...chat, ...response[0][0] };
         })
       );
 
