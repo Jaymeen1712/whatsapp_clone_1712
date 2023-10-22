913c22c9cc9959a7be5939ad906de7437e01359b
Jaymeen1712 <jaymeensonara@gmail.com>
2023-10-22 12:00:59 +0530

    vercel changes

--- client/src/api/api-client.ts
+++ client/src/api/api-client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   // baseURL: `${process.env.REACT_APP_BASE_URL}`,
-  baseURL: `https://flow-chat-backend.onrender.com`,
+  baseURL: `https://flow-chat-backend.vercel.app`,
 });
 
 export default apiClient;
